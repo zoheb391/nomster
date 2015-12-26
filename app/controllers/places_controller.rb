865b52fc54a5f32d8e713d2182aa1ed389eecb14
Zoheb <zoheb_391@hotmail.com>
2015-12-26 09:27:54 +0000

    Validating place entry

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, :only => [:new, :create, :edit, :update :destroy]
+before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy ]
 
 def index
 		@places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -10,8 +10,12 @@ def new
 end
 
 def create
-	current_user.places.create(place_params)
-	redirect_to root_path
+	@place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, :status => :unprocessable_entity
+  end
 end
 
 def show
@@ -40,6 +44,7 @@ def destroy
 	@place = Place.find(params[:id])
 	if @place.user != current_user
 		return render :text => 'Not Allowed', :status => :forbidden
+	end
 	@place.destroy
 	redirect_to root_path
 end
