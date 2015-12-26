1f00072171bf67e079ccb1607bb782c5a4b38674
Zoheb <zoheb_391@hotmail.com>
2015-12-26 08:05:26 +0000

    User restrictions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,8 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, :only => [:new, :create]
+before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 def index
-	@places = Place.paginate(:page => params[:page], :per_page => 5)
+		@places = Place.paginate(:page => params[:page], :per_page => 5)
 end
 
 def new
@@ -20,12 +20,22 @@ end
 
 def edit
 	@place = Place.find(params[:id])
+	if @place.user != current_user
+		return render :text => 'Not Allowed', :status => :forbidden
+	end
+
 end
+
 def update
 	@place =Place.find(params[:id])
+	if @place.user != current_user
+		return renter :text => 'Not Allowed', :status => :forbidden
+	end
 	@place.update_attributes(place_params)
 	redirect_to root_path
 end
+
+
 def destroy
 	@place = Place.find(params[:id])
 	@place.destroy
