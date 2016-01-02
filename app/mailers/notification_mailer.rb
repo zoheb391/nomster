64de1f9d1adf3243ea646c09ac8c4e71b0493f5c
Zoheb <zoheb_391@hotmail.com>
2016-01-02 21:04:58 +0000

    email fully loaded

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@example.com"
 
-	def comment_added
-		mail(to: "zsalam2@illinois.edu",
-		subject: "A comment has been added to your place" )
+	def comment_added(comment)
+		@place = comment.place
+		@place_owner = @place.user
+		mail(to: @place_owner.email,
+		subject: "A comment has been added to your #{@place.name}" )
 	end
 end
 
