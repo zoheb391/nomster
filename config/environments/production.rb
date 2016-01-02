fb3ed276bd6b3f718b4bc5708ec31a32979250e1
Zoheb <zoheb_391@hotmail.com>
2016-01-02 22:44:44 +0000

    added info on  heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ config.action_mailer.default_url_options = { :host => 'nomster-zoheb-salam.herok
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'nomster-zoheb-salam.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
